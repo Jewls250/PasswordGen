918d0bde963904d11c4210270bd656c3e3b7495a
Giuliano Serawop <gserawop13@gmail.com>
2020-12-18 12:15:09 -0700

    Added checkbox's and some h2's

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-confirm('Your password must include at least one of the following.')
-confirm('A lower case letter or upper case letter')
-confirm('A number or special character')
-confirm('And your password must be atlest 8 characters and not more then 128 characters')
-prompt('Enter a password!')
+
 
 
 // Write password to the #password input
@@ -19,6 +15,7 @@ function writePassword() {
 
 function generatePassword(){
   
+  prompt('Enter a password!')
 }
 
 
