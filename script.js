41b4a64f8e4f30be600869944144c33c788ac4c8
Giuliano Serawop <gserawop13@gmail.com>
2020-12-17 17:45:12 -0700

    Added confirms and prompts

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+confirm('Your password must include at least one of the following.')
+confirm('A lower case letter or upper case letter')
+confirm('A number or special character')
+confirm('And your password must be atlest 8 characters and not more then 128 characters')
+prompt('Enter a password!')
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -13,6 +20,7 @@ function writePassword() {
 function generatePassword(){
   
 }
+
 
 
 // Add event listener to generate button
