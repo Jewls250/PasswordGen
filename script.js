8b451055b62542f3f15e1119c6503a88ac84b5ab
Giuliano Serawop <gserawop13@gmail.com>
2020-12-18 18:07:01 -0700

    Generate password function complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-
+let clearBtn = document.querySelector("#clear");
 
 
 
@@ -14,11 +14,46 @@ function writePassword() {
 }
 
 function generatePassword(){
-  
-  prompt('Enter a password!')
+  confirm('You need at least one of the following')
+  confirm('A uppercase letter')
+  confirm('A lower case letter')
+  confirm('A symbol')
+  confirm('And a number')
+
+  let char = prompt('How many characters would you like?')
+  let password = "";
+  let minChar = 8
+  let maxChar = 128
+  let charRequirments  = [
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let randomNumber;
+    
+  for (let i = 0; i < char ; i++){
+    randomNumber = charRequirments[Math.floor(Math.random() * 72)]
+    password += randomNumber 
+    
+  }
+
+  if(char > minChar && char < maxChar){
+    return password
+  } else {
+    return "Character doesn't meet the requirements"
+  };
 }
 
+function resetPassword(){
+  confirm('Your password has been deleted.').reset()
+}
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+clearBtn.addEventListener("click", resetPassword)
+
+
+
+
+
+
+
+
